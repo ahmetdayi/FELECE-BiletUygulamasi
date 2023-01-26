ff025d4342ea4812a41bb730c72ec3c400d36083
Ahmet Dayi <ahmet_dayi@eskisehir.edu.tr>
2023-01-26 19:00:29 +0300

    localdatetime rearrange

--- src/main/java/com/ahmetdayi/ticketapp/entity/request/CreateTripRequest.java
+++ src/main/java/com/ahmetdayi/ticketapp/entity/request/CreateTripRequest.java
@@ -1,10 +1,11 @@
 package com.ahmetdayi.ticketapp.entity.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
@@ -14,7 +15,9 @@ import java.time.LocalDateTime;
 public class CreateTripRequest {
     @NotNull
     private double price;
-    @NotBlank
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd@HH:mm")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private LocalDateTime departureTime;
     @NotNull
     private int vehicleId;
